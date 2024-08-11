7764c820e36a71aec2192e7508c23b2d40f27bed
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-11 16:17:50 -0700

    shell

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -8,4 +8,7 @@ echo "Home directory : $HOME"
 echo "Which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "Process ID of the current shell script::$$"
-sleep 60
+#sleep 60
+sleep 60 &
+echo "Process instance ID of the background process::::::$!"
+
