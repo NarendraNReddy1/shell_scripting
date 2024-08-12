1dbd67c6b91617162032551de999cf26b2074d10
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-12 06:26:30 -0700

    ALL

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
+user_id=$(id -u)
 
-dnf install mysql -y
+if [ $user_id -ne 0 ]
+then
+    echo "Please run as root user::::"
+    exit 1
+else
+    echo "You are super user"
+fi
+
+dnf install mysql -y 
+
+if [ $? -ne 0 ]
+then
+    echo "Installed failed. pls check...."
+    exit 1
+else 
+    echo "Installation of mysql.... SUCCESS"    
+fi
+
+dnf install git -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation failed . Pls check ..."
+    exit 1
+else 
+    echo "Installation of git ... SUCCESS"
+fi        
+
+echo "Installation are over"
