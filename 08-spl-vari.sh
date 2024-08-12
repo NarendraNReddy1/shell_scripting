6550890dca5d980aa1bc012b5deb0c6ffe82c471
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-12 06:06:03 -0700

    ALL

--- 08-spl-vari.sh
+++ 08-spl-vari.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-$$ -pid
-$!- background
-$HOME 
-$PWD 
-$@ -- ALL 
-$# - number 
-$HOSTNAME 
+# $$ -pid
+# $!- background
+# $HOME 
+# $PWD 
+# $@ -- ALL 
+# $# - number 
+# $HOSTNAME 
+
+
+echo "All $@"
+echo "Count $#"
+echo "Script name: $0"
+
+echo "Home: $HOME"
+echo "Pwd: $PWD"
+echo "Hostname: $HOSTNAME"
+echo "User: $USER"
+
+echo "Executing pid:$$"
+sleep 60
+echo "Executing background: $!"
+sleep 60 &
