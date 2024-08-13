0898a9e3478f702f632a4ac2336cb2709299f38f
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-13 06:06:25 -0700

    ALL

---
+++ 14-install-packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin
+
+USER_ID=$(id -u)
+
+if [ USER_ID -ne 0 ]
+then
+    echo "Be a super user.."
+    exit 1
+else 
+    echo "You are super user...."
+fi
+
+for i in @
+do
+    echo "All packages: $i"
+done
