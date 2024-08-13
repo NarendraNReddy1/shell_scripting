a6e5552f1926792dfeb5436d347a2857b88baf7f
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-13 05:02:14 -0700

    ALL

--- 12-colors.sh
+++ 12-colors.sh
@@ -9,17 +9,17 @@ G="\e[32m"
 N="\e[0m"
 
 echo "Script started executing at: $TIMESTAMP"
-echo $R
+
 
 
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...$R FAILURE $N"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...$G SUCCESS $N"    
+        echo -e "$2...$G SUCCESS $N"    
 
     fi
 }
