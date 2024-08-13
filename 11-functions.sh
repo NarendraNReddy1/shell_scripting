c2e264ee1530b8336c5396bd836b725558754152
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-13 04:03:00 -0700

    ALL

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,31 +1,66 @@
-#!/bin/bash
+# #!/bin/bash
+
+# user_id=$(id -u)
+# TIMESTAMP=$(data +%F-%H-%M-%S)
+# SCRIPT_NAME=$($0 | cut -d "." -f1)
+# LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo "$2...FAILURE"
+#         exit
+#     else
+#         echo "$2...SUCCESS"    
+
+#     fi
+# }
+
+
+# user_id=$(id -u)
+
+# if [ $user_id -ne 0 ]
+# then
+#     echo "Please run as root user::::"
+#     exit 1
+# else
+#     echo "You are super user"
+# fi
+
+# dnf install mysql -y 
+# VALIDATE $? "Installation of MySQL"
+
+
+# dnf install git -y 
+# VALIDATE $? "Installation of GIT"
+
+
+#!/usr/bin
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
-        exit
-    else
-        echo "$2...SUCCESS"    
-
+        echo "$2....FAILURE"
+    else 
+        echo "$2....SUCCESS"
     fi
 }
 
 
-user_id=$(id -u)
+USER_ID=$(id -u)
 
-if [ $user_id -ne 0 ]
+if [ $USER_ID -ne 0 ]
 then
-    echo "Please run as root user::::"
-    exit 1
-else
-    echo "You are super user"
+    echo "Please try with super user login...."
+    exit 1 
+else 
+    echo "Now you are super user..."
 fi
 
-dnf install mysql -y 
-VALIDATE $? "Installation of MySQL"
 
+dnf install mysql -y
+VALIDATE $? "Instllation of mySQL"
 
-dnf install git -y 
-VALIDATE $? "Installation of GIT"
-
+dnf install git -y
+VALIDATE $? "Instllation of git"
