e10b41ff196eddcd2657f96c23bb7531c5ca6fff
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-13 06:19:29 -0700

    ALL

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,22 @@
 #!/usr/bin
-
 USER_ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"    
+
+    fi
+}
 
 if [ $USER_ID -ne 0 ]
 then
@@ -12,5 +28,8 @@ fi
 
 for i in $@
 do
-    echo "package: $i"
+    echo "package to install: $i"
+    dnf list installed $i &??$LOG_FILE
+    VALIDATE $? "Installation $i"
+    
 done
