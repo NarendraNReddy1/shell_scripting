ea73b85106b15e310a3252a70af29aadc4ef0bc7
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-13 18:50:12 -0700

    ALL

--- RECAP.sh
+++ RECAP.sh
@@ -4,6 +4,10 @@ PERSON1="RAMESH"
 PERSON2=$1
 echo "Enter password"
 read -s passwd
+movies=("RRR" "MOVIES")
 
 echo "Hi $PERSON1 , Please talk to $PERSON2"
 echo $passwd
+
+echo "${movies[0]}"
+echo "all: ${movies[$@]}"
