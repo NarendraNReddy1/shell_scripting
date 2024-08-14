21491eb1d7632af4b0afb5327bdbf42148813a61
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-13 19:59:10 -0700

    ALL

--- 14-install.sh
+++ 14-install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 USER_ID=$(id -u)
+TIMESTAMP$(date +%F-%M-%H-%S)
+SCRIPT_NAME=$(echo $0|cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 
 if [ $USER_ID -ne 0 ]
 then
@@ -9,9 +17,28 @@ else
     echo "You are super user"
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2..$R FAILURE $N"
+        exit 1
+    else
+        echo "$2..$G SUCCESS $N"    
+
+    fi
+}
+
 
 
 for i in $@
 do
-    echo $i
+    echo "$i to installed"
+    dnf list installed $i &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then 
+        echo "$i already installed"
+    fi 
+    else
+        dnf install $i -y &>>$LOG_FILE
+        VALIDATE $? "installation of $i"
 done
