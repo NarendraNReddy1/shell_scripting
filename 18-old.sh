f3040499d3169652e6cbb3db39be454b551b99a0
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-17 16:44:58 -0700

    ALL

--- 18-old.sh
+++ 18-old.sh
@@ -5,19 +5,19 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-PATH=/tmp/app-logs
+SOURCE_DIRECTORY=/tmp/app-logs
 
-if [ -d $PATH ]
+if [ -d $SOURCE_DIRECTORY=/tmp/app-logs ]
 then 
-    echo "$PATH exists"
+    echo "$SOURCE_DIRECTORY=/tmp/app-logs exists"
     
 else 
-    echo "$PATH does not exists"
+    echo "$SOURCE_DIRECTORY=/tmp/app-logs does not exists"
     exit 1
 fi
 # FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +10)
 
-FILES=$(find $PATH -name "*.log" -mtime +10)
+FILES=$(find $SOURCE_DIRECTORY=/tmp/app-logs -name "*.log" -mtime +10)
 
 # while IFS= read -r line
 # do 
