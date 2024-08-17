7bb4bc3ce1c83ac245097e0a59191aab9b0b1397
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-17 12:23:39 -0700

    ALL

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -18,7 +18,7 @@ DISK_THRESHOLD=5
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    FOLDER=$(echo $line | awk -F " " '{print $6F}')
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then 
         echo "$USAGE is more than $DISK_THRESHOLD for the $FOLDER"
