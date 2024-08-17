23319671fdc2ad985f080419addf27b474f5a35b
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-17 16:31:04 -0700

    ALL

--- 18-old.sh
+++ 18-old.sh
@@ -5,17 +5,17 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-PATH="/tmp/app-logs"
+PATH=/tmp/app-logs
 
 if [ -d $PATH ]
 then 
     echo "$PATH exists"
+    
 else 
     echo "$PATH does not exists"
 fi
 
 FILES=$(find $PATH -name "*.log" -mtime +10)
-#FILES=$(find $PATH  -name "*.log" -mtime +10)
 
 # while IFS= read -r line
 # do 
