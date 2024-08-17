6efb954ad34e566904cb57c66e56c4a3a94e041e
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-16 20:36:06 -0700

    ALL

--- 17-set.sh
+++ 17-set.sh
@@ -3,6 +3,12 @@ user_id=$(id -u)
 
 set -e 
 
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 if [ $user_id -ne 0 ]
 then
     echo "Please run as root user::::"
