7a68e65618d2556a7d5f59ff56c5b72b5cfa9b77
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-16 18:28:22 -0700

    ALL

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script,course::::::::::::$COURSE"
 echo "Before calling the PID of current shell script:$$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
-echo "After calling the PID of current shell script:$$"
+echo "After calling the other script,the course:::$COURSE"
