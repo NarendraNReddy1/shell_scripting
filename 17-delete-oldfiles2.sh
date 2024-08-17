cc9085e2e1752067e1f96a591503180f003504ba
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-17 16:11:53 -0700

    ALL

---
+++ 17-delete-oldfiles2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+PATH=\tmp\app-logs 
+
+if [ -d $PATH ]
+then 
+    echo "$PATH exists"
+else 
+    echo "$PATH does not exists"
+done 
+
