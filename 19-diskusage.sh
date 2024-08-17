90549be603c9ec64cec9b8ff05d4e80a567789b1
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-17 12:09:22 -0700

    ALL

---
+++ 19-diskusage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
+
+while IFS= read -r line
+do 
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%"  -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE >= $DISK_THRESHOLD ]
+    then
+        echo "$USAGE is excceds $DISK_THRESHOLD for the $FOLDER"
+    else 
+        echo "Withing range no worries"    
+    fi
+done <<< $DISK_USAGE
