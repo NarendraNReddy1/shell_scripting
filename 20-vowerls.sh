8c38e95663c44c38a5b44bcacf600655660b8105
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-18 03:34:19 -0700

    ALL

--- 20-vowerls.sh
+++ 20-vowerls.sh
@@ -3,10 +3,11 @@
 echo "Enter the string::::"
 read str1
 vowels=0
+vowel_string="aeiouAEIOU"
 
 while IFS= read -r i
 do
-    if [ $i in (aeiouAEIOU) ]
+    if [ $i in vowel_string ]
     vowels=$vowels+1
 done <<< $str1
 echo $vowels
